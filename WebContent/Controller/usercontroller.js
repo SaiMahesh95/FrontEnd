84ca4fd8b8562e172d709144bef1ba0cd644558c
MAHESH <MAHESH@DESKTOP-GRE0TUS>
2017-10-27 11:33:23 +0530

    front end

--- WebContent/Controller/usercontroller.js
+++ WebContent/Controller/usercontroller.js
@@ -1,22 +1,51 @@
 /**
  * 
  */
-app.controller('UserController'.function($scope,UserService,$location)
-		{
-
-	$scope.registerUser=function(){
-		console.log("USER DATA IS "+ $scope.user)
-		userservice.registeruser($scope.user).then(function(response){
-		console.log(response.data)
-		console.log(response.status)
-		$location.path('/home')
-		},function(response){
-			console.log(response.data)
-			console.log(response.status)
-			$scope.error=response.data
-			$location.path('/register')
-		})
-		
-		}
-		
+app.controller('UserController',function(UserService,$scope,$location,$rootScope,$cookieStore)
+        {
+    
+    $scope.register=function()
+   {
+    	console.log("register....")
+    	UserService.registerUser($scope.user).then (function(response)
+        {
+            $scope.message="Registered successfully..... Please Login...."
+                console.log("done")
+                
+                $location.path('/login')
+        }, function(response)
+        {
+            $scope.error=response.data;
+            $location.path('/registration')
+        })
+    
+    }
+    
+    $scope.login=function(){
+    	UserService.login($scope.user).then(function(response){
+            $rootScope.currentUser=response.data
+            $cookieStore.put("currentUser",response.data)
+            $location.path('/home')
+            
+        },function(response){
+            $scope.error=response.data
+            $location.path('/login')
+        })
+    }
+    $scope.userobj=UserService.getUserByUsername().then(function(response)
+    		{
+    	$scope.userobj=response.data
+    },function(response){
+    	console.log(response.status)
+    })
+    $scope.update=function()
+    {
+    	UserService.updateUserProfile($scope.userobj).then(function(response)
+    			
+    			{
+    		$scope.message="Updated the profile successfully"
+    	},function(response){
+    		console.log(response.data)
+    	})
+    }
 })
--- WebContent/Services/UserService.js
+++ WebContent/Services/UserService.js
@@ -1,21 +1,37 @@
 /**
  * 
  */
-app.factory('UserService',function($http){
-	var userService={}
-	var BASE_URL="http://localhost:"
-	
-	userService.registerUser=function(user){
-		return $http.post(BASE_URL + "/registeruser",user)
-		
-		
-	}
 
-	
-	return userService;
-	
-	
-	
-	
-	
-)}
+app.factory('UserService',function($http)
+{
+    var userService={}
+
+    userService.registerUser=function(user)
+    {
+    	console.log("registration start")
+        return $http.post("http://localhost:8081/backend-project2/registration",user)
+        console.log("Registration done")
+    }
+    
+    userService.login=function(user)
+    {
+        return $http.post("http://localhost:8081/backend-project2/login",user)
+    }
+    
+    userService.logout=function(user)
+    {
+        return $http.get("http://localhost:8081/backend-project2/logout")
+    }
+    
+    userService.getUserByUsername=function()
+    {
+        return $http.get("http://localhost:8081/backend-project2/getuserdetails")
+    }
+    
+    userService.updateUserProfile=function(user)
+    {
+        return $http.put("http://localhost:8081/backend-project2/updateprofile",user)
+    }
+    
+return userService;
+})
